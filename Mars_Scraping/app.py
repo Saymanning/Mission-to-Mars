62449cdc3661003d2fad429cabc06bf000f0573b
Saymanning <78699465+Saymanning@users.noreply.github.com>
2021-05-01 12:42:03 -0400

    10.7.1

--- Mars_Scraping/app.py
+++ Mars_Scraping/app.py
@@ -1,27 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
+
+
+#set up Flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
+# Set up App Routes and function
+# tell Flask what to display on home page
 @app.route("/")
+ 
 def index():
+# use PyMongo to find the 'mars' collection 
    mars = mongo.db.mars.find_one()
+# Flask will return an html template using the index.html file using the mars collection
    return render_template("index.html", mars=mars)
+
+# Set up 2nd App Route with function for scraping code
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
+
+# Run the code
 if __name__ == "__main__":
    app.run()
 
